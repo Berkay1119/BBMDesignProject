3895f75d4950b0b6fc3e98882f6d9fdc47335d1d
Beyza Koroglu <beyzakoroglu@otsimo.com>
2025-05-18 18:41:07 +0300

    implement tag check for colliding condition.

--- BBMDesignProject/Assets/Scripts/Backend/EasyEvent/Conditions/CollidingCondition.cs
+++ BBMDesignProject/Assets/Scripts/Backend/EasyEvent/Conditions/CollidingCondition.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using Backend.Attributes;
 using Backend.Components;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace Backend.EasyEvent.Conditions
@@ -14,6 +15,10 @@ namespace Backend.EasyEvent.Conditions
         // Needs to stay "public" otherwise it won't work
         public string firstTypeName;
         public string secondTypeName;
+        
+        public string firstRequiredTag;
+        public string secondRequiredTag;
+        
         private Type firstType, secondType;
         
         public CollidingCondition()
@@ -37,10 +42,25 @@ namespace Backend.EasyEvent.Conditions
             idx1 = Mathf.Clamp(EditorGUILayout.Popup("First Type", idx1, names), 0, names.Length - 1);
             firstTypeName = names[idx1];
             
+            // Tag 1
+            var allTags = InternalEditorUtility.tags;
+            int t1 = Array.IndexOf(allTags, firstRequiredTag);
+            firstRequiredTag = allTags[Mathf.Clamp(
+                EditorGUILayout.Popup("First Required Tag", t1, allTags),
+                0, allTags.Length - 1
+            )];
+
             // Type 2
             int idx2 = Array.IndexOf(names, secondTypeName);
             idx2 = Mathf.Clamp(EditorGUILayout.Popup("Second Type", idx2, names), 0, names.Length - 1);
             secondTypeName = names[idx2];
+            
+            // Tag 2
+            int t2 = Array.IndexOf(allTags, secondRequiredTag);
+            secondRequiredTag = allTags[Mathf.Clamp(
+                EditorGUILayout.Popup("Second Required Tag", t2, allTags),
+                0, allTags.Length - 1
+            )];
             
             GUILayout.EndVertical();
         }
@@ -79,14 +99,16 @@ namespace Backend.EasyEvent.Conditions
 
         private void OnAnyCollision(BaseComponent source, BaseComponent other)
         {
-            if (source.GetType() == firstType && other.GetType() == secondType)
-            {
-                foreach (var action in relatedEvent.Actions)
-                {
-                    action.Execute(source, other);
-                }
-                    
-            }
+            // Type check
+            if (source.GetType() != firstType || other.GetType() != secondType)
+                return;
+
+            // Tag check
+            if (!source.CompareTag(firstRequiredTag) || !other.CompareTag(secondRequiredTag))
+                return;
+
+            foreach (var action in relatedEvent.Actions)
+                action.Execute(source, other);
         }
         
     }
