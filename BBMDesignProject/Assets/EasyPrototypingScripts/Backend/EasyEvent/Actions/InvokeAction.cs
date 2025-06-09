7dbef0a9f35ff5cbacbf79402a967d848742bfd6
karakayaberkay <berkay11karakaya@gmail.com>
2025-06-09 14:11:30 +0300

    change invoke event without breaking the invoke action

---
+++ BBMDesignProject/Assets/EasyPrototypingScripts/Backend/EasyEvent/Actions/InvokeAction.cs
@@ -0,0 +1,35 @@
+﻿using Backend.Attributes;
+using Backend.Components;
+using UnityEngine;
+
+namespace Backend.EasyEvent.Actions
+{
+    public class InvokeAction:EasyAction
+    {
+        public CustomEventScriptableObject onInvoke;
+
+        public InvokeAction()
+        {
+            actionName = "Invoke Event";
+            actionDescription = "This Event invokes the UnityEvent when executed.";
+        }
+
+        public override void Execute(BaseComponent source, BaseComponent other)
+        {
+            if (onInvoke != null)
+            {
+                onInvoke.Raise();
+            }
+        }
+
+        public override void DrawGUI()
+        {
+            GUILayout.Space(10);
+            base.DrawGUI();
+            GUILayout.Space(10);
+            
+            onInvoke = (CustomEventScriptableObject)UnityEditor.EditorGUILayout.ObjectField("On Invoke", onInvoke, typeof(CustomEventScriptableObject), false);
+            GUILayout.EndVertical();
+        }
+    }
+}
